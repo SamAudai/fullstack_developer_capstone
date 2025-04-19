92d17ddc5fc9e48099d7b7510dbc8974bf09577c
Audai Alsharani <68870703+SamAudai@users.noreply.github.com>
2025-04-19 21:46:38 +0300

    Update restapis.py

--- djangoapp/restapis.py
+++ djangoapp/restapis.py
@@ -11,38 +11,40 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
+
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params + key + "=" + "&"
+    request_url = backend_url + endpoint + "?" + params
+    print("GET form {} ".format(request_url))
 
-    request_url = backend_url+endpoint+"?"+params
-
-    print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
-        # If any error occurs
-        print("Network exception occurred")
+    except requests.exceptions.RequestException as e:
+        print(f"exception {e} occurred")
+
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
+    print("request_url: ", request_url)
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
+        print("res in analyze_review_sentiments: ", response)
         return response.json()
-    except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
-        print("Network exception occurred")
+    except Exception as e:
+        print("EEEEEEEEEEEEEEEEEEEE")
+        print(f"exception {e} occurred")
+
 
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except Exception as e:
+        print(f"exception {e} occurred")
